e548eeee8fad4b28601f92653cf55a2ca70d611a
lixiang <xiang.li@spreadwin.com>
2017-03-16 15:44:34 +0800

    commit

--- app/src/main/java/com/spreawdwin/easyblur/MyApp.java
+++ app/src/main/java/com/spreawdwin/easyblur/MyApp.java
@@ -1,6 +1,8 @@
 package com.spreawdwin.easyblur;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.orhanobut.logger.Logger;
 
@@ -10,10 +12,35 @@ import com.orhanobut.logger.Logger;
  */
 
 public class MyApp extends Application {
+    private static Context sAppContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        sAppContext = this;
         Logger.init().getLogLevel();
+        initDayNightMode();
+
+        boolean isNight = SharedPreferencesUtil.getBoolean(this, Constants.ISNIGHT, false);
+
+        if (isNight) {
+            //使用夜间模式
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            //不使用夜间模式
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    private void initDayNightMode() {
+        if (MyUtils.isNightMode()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    public static Context getAppContext() {
+        return sAppContext;
     }
 }
--- app/src/main/java/com/spreawdwin/easyblur/SettingFramgent.java
+++ app/src/main/java/com/spreawdwin/easyblur/SettingFramgent.java
@@ -1,11 +1,17 @@
 package com.spreawdwin.easyblur;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.orhanobut.logger.Logger;
+
+import java.io.File;
 
 
 /**
@@ -28,6 +34,7 @@ public class SettingFramgent extends PreferenceFragment {
                 .findPreference(getString(R.string.save_num_mode));
 
         Preference cachePref = getPreferenceManager().findPreference(getString(R.string.save_cache_mode));
+        cachePref.setSummary(cachePref.getSummary() + getCacheSize());
 
         checkbox.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
 
@@ -44,7 +51,8 @@ public class SettingFramgent extends PreferenceFragment {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 Logger.d(preference.getKey());
-                return false;
+                openAppInfo(getActivity());
+                return true;
             }
         });
 
@@ -54,9 +62,29 @@ public class SettingFramgent extends PreferenceFragment {
             public boolean onPreferenceChange(Preference preference, Object newValue) {
                 boolean checked = Boolean.valueOf(newValue.toString());
                 Logger.d("Pref " + preference.getKey() + " changed to " + newValue.toString() + "checked" + checked);
+                if (checked) {
+                    SharedPreferencesUtil.setBoolean(getActivity(), Constants.ISNIGHT, true);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    SharedPreferencesUtil.setBoolean(getActivity(), Constants.ISNIGHT, false);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+                getActivity().recreate();
                 return true;
-
             }
         });
+    }
+
+    public static void openAppInfo(Context context) {
+        Intent i = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        i.addCategory(Intent.CATEGORY_DEFAULT);
+        i.setData(Uri.parse("package:" + context.getApplicationContext().getPackageName()));
+        context.startActivity(i);
+    }
+
+
+    private String getCacheSize() {
+        File file = getActivity().getApplicationContext().getCacheDir();
+        return MyUtils.getFileSize(file);
     }
 }
